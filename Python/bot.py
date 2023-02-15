4f64b796fed04d041efc657db232d3ee2c2f739a
timlopes17 <timlopes0817@gmail.com>
2023-02-15 10:07:33 -0500

    Removed unneeded files and cleaned up code. Added connectiont to database.

--- Python/bot.py
+++ Python/bot.py
@@ -1,52 +1,42 @@
 import discord
-import commands
 import json
 import os
+from supabase import create_client, Client
+import random
+from discord.ext import commands
+from typing import Optional
 
 if os.path.exists(os.getcwd() + "/config.json"):
     with open("./config.json") as f:
         configData = json.load(f)
 else:
-    configTemp = {"Token": "", "Prefix": "!"}
+    configTemp = {"DiscordToken": "", "Prefix": "!", "SupaUrl": "", "SupaKey": "", "SupaSecret": ""}
     with open(os.getcwd() + "/config.json", "w+") as f:
         json.dump(configTemp, f)
 
-async def send_message(message, user_message, is_private):
-    try:
-        response = commands.handle_command(user_message)
-        await message.author.send(response) if is_private else await message.channel.send(response)
-    except Exception as e:
-        print(e)
-
 def run_discord_bot():
-    TOKEN = configData["Token"]
-    PREFIX = configData["Prefix"]
+    DISCORD_TOKEN = configData["DiscordToken"]
+    SB_URL = configData["SupaUrl"]
+    SB_KEY = configData["SupaKey"]
+    SB_SECRET = configData["SupaSecret"]
 
-    client = discord.Client()
+    supabase: Client = create_client(SB_URL, SB_KEY)
 
-    @client.event
+    bot = commands.Bot(command_prefix="!")
+
+    @bot.event
     async def on_ready():
-        print(f'{client.user} is now running!')
+        print(f'{bot.user} is now running!')
 
-    @client.event
-    async def on_message(message): #Function is called whenever a message is sent
-        #If the message came from the bot, ignore it
-        if message.author == client.user:
-            return
+    @bot.command()
+    async def testInsert(ctx, arg1, arg2):
+        list = {'student_name': arg1, 'grade': arg2}
+        data = supabase.table("TestTable").insert(list).execute()
+        print(data)
+        await ctx.channel.send("Inserted new student")
 
-        username = str(message.author)
-        user_message = str(message.content)
-        channel = str(message.channel)
+    async def poll(ctx, prompt, opt1, opt2, opt3:Optional[str] = None, opt4:Optional[str] = None):
+        # TODO - poll command, 'prompt' is the question and the opt1 -> opt4 are the options. opt1 and 2 being required, others are optional
+        return
 
-        print(message)
-        print(f"{username} said '{user_message}' ({channel})")
-
-        #Ignores all messages without the ! indicator
-        if user_message[0] != PREFIX:
-            return
-
-        #Removes the '!' from the message
-        user_message = user_message[1:]
-        await send_message(message, user_message, is_private=False)
-
-    client.run(TOKEN)
+    bot.run(DISCORD_TOKEN)
