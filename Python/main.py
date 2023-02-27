ae267ce9688a46cafd3f2970d0f9b3fbb774d2be
timlopes17 <timlopes0817@gmail.com>
2023-02-26 22:03:28 -0500

    fixed error with api

--- Python/main.py
+++ Python/main.py
@@ -1,12 +1,5 @@
 import bot
-import os
-import threading
-
-def runApi():
-    os.system("uvicorn api:app --reload")
 
 if __name__ == '__main__':
-    x = threading.Thread(target=runApi)
-    x.start()
     bot.run_discord_bot()
 
