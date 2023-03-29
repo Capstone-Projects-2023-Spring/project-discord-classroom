f9c78d50d58cb4f8bc551e9250ddced4e3f9a11f
Ryan Klein <tug43734@temple.edu>
2023-03-28 22:27:02 -0400

    Fix comment

--- Python/utils.py
+++ Python/utils.py
@@ -1,13 +1,15 @@
 import api
 
 async def add_member_to_table(guild_id, role, nick, discord_id):
-        classroom_id = await get_classroom_id(server_id=guild_id)
+        # Obtain classroom ID
+        response = await api.get_classroom_id(server_id=guild_id)
+        classroom_id = response['id']
 
-        # Query 'User' table for member
-        response = await get_user_id(discord_id=discord_id)
+        # Query 'User' table for member and create row if member is not found
+        response = await api.get_user_id(discord_id=discord_id)
         if 'message' in response:
-            response = await create_user(nick=nick, discord_id=discord_id)
-        user_id = response['user_id']
+            response = await api.create_user(nick=nick, discord_id=discord_id)
+        user_id = response['id']
 
         # Create new row for member in 'Classroom User' table
-        await create_classroom_user(classroom_id=classroom_id, user_id=user_id, name=nick, role=role)
+        await api.create_classroom_user(classroom_id=classroom_id, user_id=user_id, name=nick, role=role)
