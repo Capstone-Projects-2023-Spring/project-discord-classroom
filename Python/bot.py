b5522d6d25c9ec3e48eb8c58b9dcf98436b88b13
Ben Baldino <tud13556@temple.edu>
2023-04-07 17:54:30 -0400

    fixed/changed how the for loop worked

--- Python/bot.py
+++ Python/bot.py
@@ -1100,18 +1100,19 @@ def run_discord_bot():
 
         return await ctx.respond("Assignment Submitted!")
 
+
     #add user for credit in discussions
-    async def add_user_to_discussion(user_id, channel):
-        points = supabase.table("Discussion").select('points').eq({'channelId': channel})
-
-
     @bot.slash_command(name = 'discussion_grade',
-                       description = '```/discussion_grade``` - when used in a discussion channel will record and give creditr to all students who participated')
+                       description = '```/discussion_grade``` - when used in a discussion channel will record and give credit to all students who participated')
     async def discussion_grade(ctx):
         #check that the command was used in a valid category and channel before working
         category = ctx.channel.category
         if category is not None and category.name == 'Discussions':
             channel = bot.get_channel(ctx.channel.id)
+            grader_id = ctx.author.id
+            score = supabase.table("Discussion").select('points').eq({'channelId': channel}).execute
+            taskId = supabase.table("Discussion").select('id').eq('channelId', channel).execute()
+            taskType = "Discussion"
 
             #makes list of all users in channel
             users = set()
@@ -1121,8 +1122,9 @@ def run_discord_bot():
                 users.add(message.author.id)
 
             #adds the users that got credit for discussion
-            for user_id in users:
-                await add_user_to_discussion(user_id, channel)
+            unique_users = set(users)
+            for student_id in unique_users:
+                supabase.table('Grade').insert({'taskType': taskType, 'graderId' : grader_id, 'taskId' : taskId, 'studentId' : student_id, 'score' : score}).execute
         else:
             # Return an error message if the command was used in a channel that is not under a category named "Discussions"
             await ctx.send(f"Sorry, this command can only be used in channels under a category named Discussions.") 
