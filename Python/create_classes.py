b7487e5da7a8a6a22a80c62fc10321a67dd99082
Ryan Klein <tug43734@temple.edu>
2023-04-12 09:56:36 -0400

    Update classes

--- Python/create_classes.py
+++ Python/create_classes.py
@@ -2,16 +2,42 @@ from typing import List, Any
 from pydantic import BaseModel
 
 
-class Quiz(BaseModel):
+
+class Assignment(BaseModel):
+    id: Optional[int] = None
+    channelId: int
+    points: int
+    startDate: datetime.date
+    dueDate: datetime.date
+    classroomId: int
     title: str
-    points: float
-    start: str
-    due: str
-    time: int
-    questions: str
+
+class Classroom(BaseModel):
+    id: Optional[int] = None
+    attendance: int
+    serverId: int
+    serverName: str
+
+class Classroom_User(BaseModel):
+    classroomId: int
+    role: str
+    userId: int
+    attendance: Optional[int] = None
+
+class Discussion(BaseModel):
     classroomId: int
     channelId: int
+    title: str
+    points: int
+    startDate: datetime.date
+    dueDate: datetime.date
 
+class Grade(BaseModel):
+    taskType: str
+    graderId: int
+    taskId: int
+    studentId: int
+    score: int
 
 class Question(BaseModel):
     question: str
@@ -19,26 +45,17 @@ class Question(BaseModel):
     wrong: List[str]
     points: float
 
-
-class Discussion(BaseModel):
-    title: str
-    points: int
-    start: str
-    due: str
+class Quiz(BaseModel):
+    questions: str
     channelId: int
+    title: str
+    points: float
+    startDate: datetime.date
+    dueDate: datetime.date
+    timeLimit: int
     classroomId: int
 
-class Assignment(BaseModel):
-    title: str
-    start: str
-    due: str
-    points: int
-    classroomId: int
-    channelId: int
-
-
-class Grade(BaseModel):
-    graderId: int
-    taskId: int
-    studentId: int
-    score: int
+class User(BaseModel):
+    id: Optional[int] = None
+    name: str
+    discordId: int
