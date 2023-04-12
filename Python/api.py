0e1f5e24d880e6f4055eaf2581d794069736ebc3
Ryan Klein <tug43734@temple.edu>
2023-04-12 09:56:56 -0400

    Move classes to create_classes.py

--- Python/api.py
+++ Python/api.py
@@ -57,69 +57,6 @@ supabase = create_client(
 
 
 #
-# Models
-#
-
-class Assignment(BaseModel):
-    id: Optional[int] = None
-    channelId: int
-    points: int
-    startDate: datetime.date
-    dueDate: datetime.date
-    classroomId: int
-    title: str
-
-class Classroom(BaseModel):
-    id: Optional[int] = None
-    attendance: int
-    serverId: int
-    serverName: str
-
-class Classroom_User(BaseModel):
-    classroomId: int
-    role: str
-    userId: int
-    attendance: Optional[int] = None
-
-class Discussion(BaseModel):
-    classroomId: int
-    channelId: int
-    title: str
-    points: int
-    startDate: datetime.date
-    dueDate: datetime.date
-
-class Grade(BaseModel):
-    taskType: str
-    graderId: int
-    taskId: int
-    studentId: int
-    score: int
-
-class Question(BaseModel):
-    question: str
-    answer: str
-    wrong: List[str]
-    points: float
-
-class Quiz(BaseModel):
-    questions: str
-    channelId: int
-    title: str
-    points: float
-    startDate: datetime.date
-    dueDate: datetime.date
-    timeLimit: int
-    classroomId: int
-
-class User(BaseModel):
-    id: Optional[int] = None
-    name: str
-    discordId: int
-
-
-
-#
 # API Endpoints
 #
 
@@ -318,7 +255,7 @@ async def get_user(discord_id: int):
         return user
 
 @app.get("/user/{discord_id}/id")
-async def get_user_id(discord_id: int, response_model=User, response_model_include={'id'}):
+async def get_user_id(discord_id: int):
     sb_response = supabase.table('User').select('*').eq('discordId', discord_id).execute()
     if sb_response.data == []:
         return JSONResponse(content={'message': 'user not found'})
